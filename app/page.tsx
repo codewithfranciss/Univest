e50a66a91d05750d1d5e3fc79d1966ceb989b654
Dozie <offor919@gmail.com>
2026-01-11 17:18:29 +0100

    redesign

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,1 @@
-import Image from "next/image";
-import Header from "@/components/waitlist/Header";
-import Hero from "@/components/waitlist/Hero";
-import HowItWorks from "@/components/waitlist/HowItWorks";
-import WaitlistForm from "@/components/waitlist/WaitlistForm";
-export default function Home() {
-  return (
-    <div className="min-h-screen bg-background">
-      <Header />
-        <Hero />
-        <HowItWorks />
-        <WaitlistForm />
-    </div>
-  );
-}
+
